3e036bbfbfc03e76be2ee906da702eeb5871a3f7
Wojciech Rusiecki <miecze123@onet.pl>
2023-09-02 00:52:06 +0200

    Added overloaded methods, still need to implement them

--- HotDeskBooking/Interfaces/IDesks.cs
+++ HotDeskBooking/Interfaces/IDesks.cs
@@ -7,10 +7,13 @@ namespace HotDeskBooking.Interfaces
     {
         Task<bool> AddDesk(Desk desk);
         Task<bool> DeleteDesk(int id);
+        Task<Desk> GetDesk(int id, bool isAdmin);
         Task<Desk> GetDesk(int id);
         Task<List<Desk>> GetAvailableDesks(DateTime? startDateTime, DateTime? endDateTime);
         Task<List<Desk>> GetAvailableDesksByOffice(int officeId, DateTime? startDateTime, DateTime? endDateTime);
+        Task<List<Desk>> GetDesks(bool isAdmin);
         Task<List<Desk>> GetDesks();
+        Task<List<Desk>> GetDesksByOffice(int officeId, bool isAdmin);
         Task<List<Desk>> GetDesksByOffice(int officeId);
         Task<bool> ChangeAvailability(int id);
     }
